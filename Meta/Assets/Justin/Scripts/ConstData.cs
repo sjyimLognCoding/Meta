b30d799adeecd1b1eaf4b28effbaf16b48674cac
Yim <sjyim@logncoding.com>
2023-10-07 16:59:36 +0900

    Leave Note Feature - partially done
    todo : load from json

---
+++ Meta/Assets/Justin/Scripts/ConstData.cs
@@ -0,0 +1,17 @@
+
+using System.Collections.Generic;
+
+public static class ConstData
+{
+    public static string[] ItemType = new string[]
+    {
+        "Table",
+        "Chair"
+    };
+
+    public static Dictionary<string, int> ItemPrice = new Dictionary<string, int>
+    {
+        {"Table", 1000},
+        {"Chair", 200}
+    };
+}
---
+++ Meta/Assets/Justin/Scripts/NoteHolder.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public struct Note
+{
+    public string ThisNote;
+    public int Price;
+    public string ItemType;
+}
+
+
+
+
+public class NoteHolder : MonoBehaviour
+{
+    public Note myNote;
+
+    private void Start()
+    {
+        myNote.Price = ConstData.ItemPrice["Table"];
+        myNote.ItemType = ConstData.ItemType[0];
+    }
+}
